5a102cc795dffde15d33dfea60e5a38afb4e2064
Sirin <ahmet.sirin@piri1513.com.tr>
2024-11-15 01:09:19 +0300

    employee ile user tabloları user_id üzerinden etkileşimli yapıldı

--- backend/uav_manufacturing_application/models.py
+++ backend/uav_manufacturing_application/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # İHA Modeli
 class UAV(models.Model):
@@ -27,7 +29,7 @@ class Part(models.Model):
     name = models.CharField(max_length=50,default=None)
     uav_type = models.ForeignKey(UAVType, on_delete=models.CASCADE,default=None)  # Parçanın ait olduğu iha tipi
     part_type = models.ForeignKey(PartType, on_delete=models.CASCADE,default=None)  # Parçanın türü 
-    uav = models.ForeignKey(UAV, on_delete=models.CASCADE,default=None)  # Part yalnızca bir UAV ile ilişkilendirilecek
+    uav = models.ForeignKey(UAV, on_delete=models.CASCADE,default=None)  # Part yalnızca bir UAV ile ilişkili
 
     def __str__(self):
         return f"{self.name} ({self.part_type.name} - {self.uav_type.name})"
@@ -36,15 +38,16 @@ class Part(models.Model):
 class Team(models.Model):
     name = models.CharField(max_length=100,default=None)
     is_montage_team = models.BooleanField(default=False) # Takım montaj takımı mı?
-    part_type = models.ForeignKey(PartType, on_delete=models.CASCADE,default=None)  # Her takım yalnızca belirli bir türde parça üretir
+    part_type = models.ForeignKey(PartType, on_delete=models.CASCADE, null=True, blank=True)  # Her takım yalnızca belirli bir türde parça üretir, montaj takımı için belirtilmez
 
     def __str__(self):
         return self.name
 
-# Employee Modeli
+# Employee Modeli.name
 class Employee(models.Model):
-    name = models.CharField(max_length=50,default=None)
-    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='employees',default=None)  # Her çalışan bir takıma atanabilir
+    user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
+    name = models.CharField(max_length=50)
+    team = models.ForeignKey('Team', on_delete=models.CASCADE, related_name='employee', null=True) # Her çalışan bir takıma atanabilir
 
     def __str__(self):
         return self.name
